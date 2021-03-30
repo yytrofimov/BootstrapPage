ba12f503bd29ec3e18f88ba605fca0beebb0fb9f
Yuriy Trofimov <70945999+yytrofimov@users.noreply.github.com>
2021-03-30 13:11:01 +0300

    test point

--- js/courses.js
+++ js/courses.js
@@ -1,4 +1,4 @@
-function createCoursesTable(dataset, requiredData, outputDivID) {
+function createCoursesOut(dataset, requiredData, outputDivID) {
     const rate = 18;
     let table = document.createElement("table");
     table.className = "table";
@@ -19,12 +19,12 @@ function createCoursesTable(dataset, requiredData, outputDivID) {
     let tableBody = document.createElement("tbody");
     table.appendChild(tableBody);
 
-    let sum = 0;
+    let points = [];
     for (sample of dataset) {
         let tableString = document.createElement("tr");
         for (label of requiredData) {
             if (label != "Kurs") {
-                sum += sample[label];
+                points.push(sample[label]);
             }
             let stringCell = document.createElement("th");
             stringCell.innerHTML += sample[label];
@@ -36,24 +36,20 @@ function createCoursesTable(dataset, requiredData, outputDivID) {
         tableBody.append(tableString);
     }
 
-
-
     let tableString = document.createElement("tr");
     let stringCell = document.createElement("th");
     stringCell.innerHTML += "Totals";
     tableString.appendChild(stringCell);
 
-
     for (let i = 0; i < requiredData.length - 1; i++) {
         let stringCell = document.createElement("th");
-        stringCell.innerHTML += sum;
+        stringCell.innerHTML += points.reduce((a, b) => a + b, 0);
         tableString.appendChild(stringCell);
     }
     stringCell = document.createElement("th");
-    stringCell.innerHTML += sum * rate;
+    stringCell.innerHTML += points.reduce((a, b) => a + b, 0) * rate;
     tableString.appendChild(stringCell);
     tableBody.append(tableString);
-
 
     let outDiv = document.getElementById(outputDivID);
     outDiv.appendChild(table);
@@ -69,60 +65,52 @@ function axiosGetJSON(pathToFile, requiredData, outputDivID) {
         for (i of response.data) {
             dataset.push(i);
         }
-        createCoursesTable(dataset, requiredData, outputDivID);
+        createCoursesOut(dataset, requiredData, outputDivID);
     });
     gifimg.src = "";
     return dataset;
 }
 
-
-
-function handleJSON(pathToFile,requiredData,outputDivID,outputCanvasID) {
-    axiosGetJSON(pathToFile,requiredData,outputDivID)
-    
-
-}
 axiosGetJSON("data/courses_1 copy.json", ["Kurs", "Kunskapspoäng (kp)"], "output_1");
 
-
 function createCoursesDiagram(outputDivID) {
-    let ctx = document.getElementById("output_2")
+    let ctx = document.getElementById("output_2");
     data = {
-        datasets: [{
-            data: [10, 20, 30]
-        }],
-    
+        datasets: [
+            {
+                data: [10, 20, 30],
+            },
+        ],
+
         // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: [
-            'Red',
-            'Yellow',
-            'Blue'
-        ]
+        labels: ["Red", "Yellow", "Blue"],
     };
-    options = {}
+    options = {};
     let myPieChart = new Chart(ctx, {
-        type: 'pie',
+        type: "pie",
         data: data,
-        options: options
+        options: options,
     });
 }
 
 // createCoursesDiagram('output_2')
 
 new Chart(document.getElementById("pie-chart"), {
-    type: 'pie',
+    type: "pie",
     data: {
-      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-      datasets: [{
-        label: "Population (millions)",
-        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-        data: [2478,5267,734,784,433]
-      }]
+        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+        datasets: [
+            {
+                label: "Population (millions)",
+                backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                data: [2478, 5267, 734, 784, 433],
+            },
+        ],
     },
     options: {
-      title: {
-        display: true,
-        text: 'Predicted world population (millions) in 2050'
-      }
-    }
+        title: {
+            display: true,
+            text: "Predicted world population (millions) in 2050",
+        },
+    },
 });
