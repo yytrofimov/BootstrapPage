07b204a4263e3912b8c7ef5e91591a601677364b
Yuriy Trofimov <70945999+yytrofimov@users.noreply.github.com>
2021-03-30 16:34:30 +0300

    to-do styles updated

--- js/to-do.js
+++ js/to-do.js
@@ -28,14 +28,7 @@ function renderTodos(todos) {
         const checked = item.completed ? "checked" : null;
         const li = document.createElement("li");
         li.setAttribute("class", "item");
-        // <li class="item" data-key="20200708"> </li>
         li.setAttribute("data-key", item.id);
-        /* <li class="item" data-key="20200708"> 
-          <input type="checkbox" class="checkbox">
-          Go to Gym
-          <button class="delete-button">X</button>
-        </li> */
-        // if item is completed, then add a class to <li> called 'checked', which will add line-through style
         if (item.completed === true) {
             li.classList.add("checked");
         }
@@ -45,36 +38,26 @@ function renderTodos(todos) {
       ${item.name}
       <button class="delete-button">X</button>
     `;
-        // finally add the <li> to the <ul>
         todoItemsList.append(li);
     });
 }
 
-// function to add todos to local storage
 function addToLocalStorage(todos) {
-    // conver the array to string then store it.
     localStorage.setItem("todos", JSON.stringify(todos));
-    // render them to screen
     renderTodos(todos);
 }
 
-// function helps to get everything from local storage
 function getFromLocalStorage() {
     const reference = localStorage.getItem("todos");
-    // if reference exists
     if (reference) {
-        // converts back to array and store it in todos array
         todos = JSON.parse(reference);
         renderTodos(todos);
     }
 }
 
-// toggle the value to completed and not completed
 function toggle(id) {
     todos.forEach(function (item) {
-        // use == not ===, because here types are different. One is number and other is string
         if (item.id == id) {
-            // toggle the value
             item.completed = !item.completed;
         }
     });
@@ -82,32 +65,20 @@ function toggle(id) {
     addToLocalStorage(todos);
 }
 
-// deletes a todo from todos array, then updates localstorage and renders updated list to screen
 function deleteTodo(id) {
-    // filters out the <li> with the id and updates the todos array
     todos = todos.filter(function (item) {
-        // use != not !==, because here types are different. One is number and other is string
         return item.id != id;
     });
-
-    // update the localStorage
     addToLocalStorage(todos);
 }
 
-// initially get everything from localStorage
 getFromLocalStorage();
 
-// after that addEventListener <ul> with class=todoItems. Because we need to listen for click event in all delete-button and checkbox
 todoItemsList.addEventListener("click", function (event) {
-    // check if the event is on checkbox
     if (event.target.type === "checkbox") {
-        // toggle the state
         toggle(event.target.parentElement.getAttribute("data-key"));
     }
-
-    // check if that is a delete-button
     if (event.target.classList.contains("delete-button")) {
-        // get id from data-key attribute's value of parent <li> where the delete-button is present
         deleteTodo(event.target.parentElement.getAttribute("data-key"));
     }
 });
